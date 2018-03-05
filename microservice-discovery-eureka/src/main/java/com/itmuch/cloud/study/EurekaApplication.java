a535e292faa51260b231278a2164b0281086ff0b
Tom <424222352@qq.com>
2018-03-05 22:41:09 +0800

    demo 01

---
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Mr Tom
+ * @date: 2018/3/5
+ * @since: 2018/3/5
+ * @email: ptomjie@gmail.com
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
